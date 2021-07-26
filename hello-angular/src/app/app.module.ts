55d1c126725ec6ca9d68e0a0617059f54da1e714
vjt4 <vtrotter@u.rochester.edu>
2021-07-26 18:43:55 -0500

    bootstrapping in progress

--- hello-angular/src/app/app.module.ts
+++ hello-angular/src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule } from '@angular/router/src/router_module';
+import { SharedBootstrapModule } from './shared/shared-bootstrap.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent
-  ],
+  declarations: [AppComponent, LoginComponent, NavbarComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    FormsModule,
+    SharedBootstrapModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
