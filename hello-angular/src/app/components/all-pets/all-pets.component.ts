a3cf390f2c3a7e16a77a43a00a94215c41675f8e
cbellaro <cbellaro@gmail.com>
2021-07-28 12:37:04 -0400

    tweaked pet cards a bit

--- hello-angular/src/app/components/all-pets/all-pets.component.ts
+++ hello-angular/src/app/components/all-pets/all-pets.component.ts
@@ -9,19 +9,31 @@ export class AllPetsComponent implements OnInit {
 
   pet1 = {
     id: 1,
-    content: "dog",
+    name:"Rex",
+    species: "dog",
+    breed:"Golden Retriever",
+    age:"young",
+    size:"large",
     img: "https://ewscripps.brightspotcdn.com/dims4/default/5b1b324/2147483647/strip/true/crop/2218x1248+0+85/resize/1280x720!/quality/90/?url=https:%2F%2Fewscripps.brightspotcdn.com%2F40%2Fe3%2F160ee9db4358815e56073352b7b6%2Fdcm-dog-diets-2.png"
   }
 
   pet2 = {
     id: 2,
-    content: "cat",
+    name:"Goldie",
+    species: "fish",
+    breed:"Goldfish",
+    age:"young",
+    size:"small",
     img: "https://ewscripps.brightspotcdn.com/dims4/default/5b1b324/2147483647/strip/true/crop/2218x1248+0+85/resize/1280x720!/quality/90/?url=https:%2F%2Fewscripps.brightspotcdn.com%2F40%2Fe3%2F160ee9db4358815e56073352b7b6%2Fdcm-dog-diets-2.png"
   }
 
   pet3 = {
     id: 3,
-    content: "fish",
+    name:"Callie",
+    species: "cat",
+    breed:"Calico",
+    age:"old",
+    size:"small",
     img: "https://ewscripps.brightspotcdn.com/dims4/default/5b1b324/2147483647/strip/true/crop/2218x1248+0+85/resize/1280x720!/quality/90/?url=https:%2F%2Fewscripps.brightspotcdn.com%2F40%2Fe3%2F160ee9db4358815e56073352b7b6%2Fdcm-dog-diets-2.png"
   }
 
