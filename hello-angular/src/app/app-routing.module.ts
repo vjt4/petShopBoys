0cf6a6168e09193a9d8b0a9aeea0933f382718db
Chris <cbellaro@gmail.com>
2021-07-29 17:30:09 -0400

    Revert "worked on some frontend"

--- hello-angular/src/app/app-routing.module.ts
+++ hello-angular/src/app/app-routing.module.ts
@@ -1,26 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
-import { SearchpetsComponent } from './components/searchpets/searchpets.component';
 
-const routes: Routes = [
-  
-  {
-    path:'login',
-    component:LoginComponent
-  },
-  
-  {
-    path:'', 
-    component:LoginComponent
-  },
-  
-  {
-    path:'searchpets',
-    component:SearchpetsComponent
-  }
-
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
