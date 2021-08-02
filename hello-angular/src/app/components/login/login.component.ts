173812b7163bbcc14f64c1206931b7cb341f8ed0
cbellaro <cbellaro@gmail.com>
2021-08-01 23:32:46 -0400

    made some progress on sign in function

--- hello-angular/src/app/components/login/login.component.ts
+++ hello-angular/src/app/components/login/login.component.ts
@@ -3,6 +3,7 @@ import { LoginService } from 'src/app/services/login.service';
 import { User } from 'src/app/models/user';
 import { Userlogin } from 'src/app/models/userlogin';
 import { Router } from '@angular/router';
+import { SignupService } from 'src/app/services/signup.service';
 
 @Component({
   selector: 'app-login',
@@ -13,10 +14,16 @@ export class LoginComponent implements OnInit {
 
   public username:string = "";
   public password:string = "";
+  public active:boolean = true;
+  public first_name:string = "";
+  public last_name:string = "";
+  public email:string = "";
+  public isAdmin:boolean = false;
   public userLogin:any = null;
+  public newUser:any = null;
   hiddenToggle:boolean = true;
 
-  constructor(private LoginService: LoginService, private router: Router) {}
+  constructor(private LoginService: LoginService, private SignupService: SignupService, private router: Router) {}
 
 
   ngOnInit(): void {}
@@ -42,6 +49,34 @@ export class LoginComponent implements OnInit {
           this.hiddenToggle = false;
         }
       )
+
+  }
+
+  signUp():void {
+    let userCredentials:User = {
+
+      username: this.username,
+      password: this.password,
+      active: this.active,
+      first_name: this.first_name,
+      last_name: this.last_name,
+      email: this.email,
+      isAdmin: this.isAdmin
+    }
+    
+    console.log(userCredentials);
+    this.SignupService.signUp(userCredentials).subscribe(
+      (data:User) => {
+        this.newUser = data;
+        console.log(this.newUser);
+      },
+
+      () => {
+        this.newUser = null;
+        console.log("couldn't sign up")
+        this.hiddenToggle = false;
+      }
+    )
 
   }
 
