ccf4f32bff926d0fd573392718f7ed661da016ec
vjt4 <vtrotter@u.rochester.edu>
2021-08-01 20:17:44 -0500

    single pet navigation sends to old/dummypage

--- hello-angular/src/app/models/pets.ts
+++ hello-angular/src/app/models/pets.ts
@@ -1,14 +1,15 @@
-import { Binary } from "@angular/compiler";
+import { Binary } from '@angular/compiler';
 
 export class Pets {
-    constructor(
-        private name:string,
-        private species:string,
-        private breed:string,
-        private age:string,
-        private gender:string,
-        private size:string,
-        private price:number,
-        private picture:string,
-    ){}
+  constructor(
+    private animalId: number,
+    private name: string,
+    private species: string,
+    private breed: string,
+    private age: string,
+    private gender: string,
+    private size: string,
+    private price: number,
+    private picture: string
+  ) {}
 }
