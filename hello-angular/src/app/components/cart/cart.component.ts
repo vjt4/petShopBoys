0d8d021d25435d335a71f1c2211798ce8619f4c9
vjt4 <vtrotter@u.rochester.edu>
2021-08-03 16:27:04 -0500

    cart service purchase function done

--- hello-angular/src/app/components/cart/cart.component.ts
+++ hello-angular/src/app/components/cart/cart.component.ts
@@ -1,36 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { Pets } from 'src/app/models/pets';
 import { CartService } from 'src/app/services/cart.service';
 import { PetsService } from 'src/app/services/pets.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
-
   items = this.CartService.getItems();
   totalamount = 0;
 
-
-  constructor(private CartService:CartService) { }
+  constructor(private CartService: CartService) {}
 
   ngOnInit(): void {
     this.getTotal();
+    //let cart: Pets = {
 
+    //}
   }
-  
-  getTotal(){
+
+  getTotal() {
     let total = 0;
 
     for (var i = 0; i < this.items.length; i++) {
-        if (this.items[i].price) {
-            total += this.items[i].price;
-            this.totalamount = total;
-        }
+      if (this.items[i].price) {
+        total += this.items[i].price;
+        this.totalamount = total;
+      }
     }
-    console.log(total)
+    console.log(total);
     return total;
-}
-
+  }
 }
--- hello-angular/src/app/services/cart.service.ts
+++ hello-angular/src/app/services/cart.service.ts
@@ -5,24 +5,30 @@ import { Pets } from '../models/pets';
 import { User } from '../models/user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
+  constructor(private http: HttpClient) {}
 
   items: Pets[] = [];
-  
-    addToCart(pets: Pets) {
-      this.items.push(pets);
-    }
-  
-    getItems() {
-      return this.items;
-    }
-    
-  
-    clearCart() {
-      this.items = [];
-      return this.items;
-    }
-  
+
+  addToCart(pets: Pets) {
+    this.items.push(pets);
   }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+
+  purchase(cart: any): Observable<Pets> {
+    console.log(cart);
+    return this.http.delete(
+      'http://localhost:8080/PetShop/checkout, cart'
+    ) as Observable<Pets>;
+  }
+}
